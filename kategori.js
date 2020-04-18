7e4b6f835754e9e242699d4497f8624b0566c008
mauludani <63839982+mauludani@users.noreply.github.com>
2020-04-18 13:44:46 +0700

    Update kategori.js

--- kategori.js
+++ kategori.js
@@ -2,6 +2,7 @@ let xhr = new XMLHttpRequest();
         xhr.onreadystatechange = function(){
             if(xhr.readyState == 4 && xhr.status == 200){
                 let aplikasi = JSON.parse(this.responseText);
+                    aplikasi.sort(GetSortOrder("tglUpdate"));  
                 for (var i in aplikasi) {
                     if (aplikasi[i]['namaApps']==namaApps) {
                         const isi = document.querySelector('.oldversion');
@@ -14,5 +15,15 @@ let xhr = new XMLHttpRequest();
                 }
             }
         }
+function GetSortOrder(prop) {  
+    return function(a, b) {  
+        if (a[prop] < b[prop]) {  
+            return 1;  
+        } else if (a[prop] > b[prop]) {  
+            return -1;  
+        }  
+        return 0;  
+    }  
+}  
 xhr.open('GET', 'https://raw.githubusercontent.com/mauludani/project/master/kategori.json', true);
 xhr.send();
